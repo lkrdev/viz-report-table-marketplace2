68f6acb92b265c7a043d0fe1151a240d80317bae
bwebs <bfweber@gmail.com>
2025-08-08 11:53:25 -0700

    Remove report_table.js and associated files; update webpack config to output to dist/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/report_table.js',
   output: {
     filename: 'report_table.js',
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname + '/dist'),
   },
   devtool: 'source-map',
   module: {
